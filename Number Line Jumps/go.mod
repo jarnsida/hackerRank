24ff9db9cfd371c92e8d2e1b2ba3bf81d3632282
Eugene Sviat <oceaos@gmail.com>
2021-08-12 14:57:19 +0300

    go mod

---
+++ Number Line Jumps/go.mod
@@ -0,0 +1,3 @@
+module "github.com/jarnsida/hackerRank/Number Line Jumps"
+
+go 1.16
--- Number Line Jumps/main.go
+++ Number Line Jumps/main.go
@@ -34,11 +34,8 @@ func kangaroo(x1 int32, v1 int32, x2 int32, v2 int32) string {
 			} else if x1 > x2 {
 				return "NO"
 			}
-
 		}
-
 	}
-
 }
 
 func main() {
